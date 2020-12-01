f721e34854d1d6a8242ead2850f085f6f07df959
Nicolas <nicolas.nguyen@epitech.eu>
2020-12-01 09:55:38 +0100

    corection merge

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using UnityEngine;
 
 public class Bullet : MonoBehaviour
@@ -6,18 +5,15 @@ public class Bullet : MonoBehaviour
     private Transform target;
 
     public float speed = 70f;
-
     public GameObject impactEffect;
 
-    public void Seek (Transform _target)
+    public void Seek(Transform _target)
     {
         target = _target;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(target == null)
+    void Update() {
+        if (target == null)
         {
             Destroy(gameObject);
             return;
@@ -31,33 +27,15 @@ public class Bullet : MonoBehaviour
             HitTarget();
             return;
         }
-        transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+
+        transform.Translate (dir.normalized * distanceThisFrame, Space.World);
+
     }
 
     void HitTarget()
     {
-        GameObject effectIns = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
+        GameObject effectIns = Instantiate(impactEffect, transform.position, transform.rotation);
         Destroy(effectIns, 2f);
-        //Destroy(target.gameObject);
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Bullet : MonoBehaviour
-{
-    public GameObject hitEffect;
-    public int damage = 5;
-
-    void OnCollisionEnter(Collision col) {
-        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
-        if (col.gameObject.GetComponent<DamageableObject>())
-        {
-            Debug.Log("hit " + col.gameObject.name);
-            col.gameObject.GetComponent<DamageableObject>().takeDamage(damage, gameObject);
-        }
-        Destroy(effect, 5f);
->>>>>>> 8009c49b7bea6b6b52dcf1ffa23a909e67c5e5c4
         Destroy(gameObject);
     }
 }

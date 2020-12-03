a3b9454009424cf333ed9916907b2190d5f8db4b
Iliou <ilias.grosy@epitech.eu>
2020-12-03 17:37:05 +0900

    add Level manager. Stille missing: unlocking levels when succeeded previous one

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour
 {
     public AudioMixer audiomixer;
+    public Button[] lvlButtons;
+
+    void Start()
+    {
+        int lvlAt = PlayerPrefs.GetInt("levelAt", 1);
+
+        for (int i = 0; i < lvlButtons.Length; ++i)
+        {
+            if (i + 1 > lvlAt)
+            {
+                lvlButtons[i].interactable = false;
+            }
+
+        }
+    }
 
     private void Awake() {
         HardMode.hardModeActivated = false;
@@ -15,11 +31,6 @@ public class SceneLoader : MonoBehaviour
     public void ActiveHardMode()
     {
         HardMode.hardModeActivated = !HardMode.hardModeActivated;
-    }
-
-    public void LoadGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);   
     }
 
     public void QuitGame()
